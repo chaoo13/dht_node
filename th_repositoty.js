6d221fa9941c5009f5b830771582ec6f5a212074
겁나남편 <chaoo.kts@gmail.com>
2021-04-13 11:34:17 +0900

    modified db path

--- th_repositoty.js
+++ th_repositoty.js
@@ -11,15 +11,15 @@ class ThRepository {
         return this.dao.all('Select cdate, chour, cminute, avgtemp, avghumid from tbsummary where cdate = ?', [qDate]);
     }    
     getLastTenTh() {
-        return this.dao.all('Select rowid, temp, humid, ctime from (Select rowid, temp, humid, ctime from tbth2 order by ctime desc limit 10) a order by ctime asc', []);
+        return this.dao.all('Select rowid, temp, humid, ctime from (Select rowid, temp, humid, ctime from tbth order by ctime desc limit 10) a order by ctime asc', []);
     }
 
     getLastTenAfterRowid(rowId) {
-        return this.dao.all('Select rowid, temp, humid, ctime from tbth2 where rowid > ? order by ctime asc limit 10', [rowId]);
+        return this.dao.all('Select rowid, temp, humid, ctime from tbth where rowid > ? order by ctime asc limit 10', [rowId]);
     }
 
     makeYesterdaySummary() {
-        return this.dao.run("insert into tbsummary SELECT strftime('%Y-%m-%d',ctime) as cdate, strftime('%H',ctime) AS chour, (cast(strftime('%M',ctime) as integer) / 30) * 30 as cminute , round(AVG(temp),1) AS avgtemp , round(AVG(humid),1) as avghumid from tbth2 Where strftime('%Y-%m-%d',ctime) = strftime('%Y-%m-%d', date('now','-1 day')) group by cdate, chour, cminute;", []);
+        return this.dao.run("insert into tbsummary SELECT strftime('%Y-%m-%d',ctime) as cdate, strftime('%H',ctime) AS chour, (cast(strftime('%M',ctime) as integer) / 30) * 30 as cminute , round(AVG(temp),1) AS avgtemp , round(AVG(humid),1) as avghumid from tbth Where strftime('%Y-%m-%d',ctime) = strftime('%Y-%m-%d', date('now','-1 day')) group by cdate, chour, cminute;", []);
     }
 
 }
